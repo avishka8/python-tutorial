8275cc963cd6e83f963290aa9f67bef0e6d001c0
avishka8 <avishka1108@gmail.com>
2025-01-22 22:59:33 +0530

    secondfile

--- second.py
+++ second.py
@@ -8,3 +8,25 @@ print(str2+str3)
 
 #length
 print(len(str1), len(str2), len(str3), len(str2+str3))
+
+#indexing
+print(str1[0])
+print(str1[4])
+print(str1[19])
+print(str1[45])
+
+#slicing
+print(str1[0:4], str1[:4])
+print(str1[5:])
+#negative slicing
+print(str1[-5:-1])
+print(str1[-10:])
+
+#strfunctions
+print(str1.endswith("on"))
+print(str2.endswith("abs"))
+print(str3.capitalize())
+print(str1.replace("t","x"))
+print(str1.find("is"))
+print(str1.find("q"))
+print(str1.count("t"))
