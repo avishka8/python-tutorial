ed815911cc8712639f0c8ebf4417e2414d8b3daa
avishka8 <avishka1108@gmail.com>
2025-01-22 23:18:56 +0530

    python

--- second.py
+++ second.py
@@ -30,3 +30,31 @@ print(str1.replace("t","x"))
 print(str1.find("is"))
 print(str1.find("q"))
 print(str1.count("t"))
+
+#conditional statements
+age=int(input("enter age"))
+if(age>18):
+    print("eligible for driving license")
+elif(age==18):
+    print("can get learner's license")    
+else:
+    print("not eligible")
+
+#odd-even check
+num=int(input("enter number "))
+if num%2==0:
+    print("Number is even") 
+else:
+    print("Number is odd")       
+
+#q2
+a=4
+b=6
+c=2
+if (a>b and a>c):
+    print(a,"is greatest")
+elif (b>c and b>a):
+    print(b,"is greatest")
+else:
+    print(c,"is greatest")
+
