ce29cf47017011df6f346c75f2c2dc420797dc50
avishka8 <avishka1108@gmail.com>
2025-01-22 22:45:01 +0530

    secondfile

--- second.py
+++ second.py
@@ -1,6 +1,10 @@
 str1="this is a string.\nwe are creating it in python"
 print(str1)
+
 #concatenation
 str2="hello "
 str3="world"
 print(str2+str3)
+
+#length
+print(len(str1), len(str2), len(str3), len(str2+str3))
