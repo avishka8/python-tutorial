c6ff819f85e54f254eaf033ede7d6a2f2bd7e1f8
avishka8 <avishka1108@gmail.com>
2025-02-07 05:49:25 +0530

    practicing oops

--- oops.py
+++ oops.py
@@ -11,19 +11,28 @@ class Student:
         self.name=name
         self.marks=marks
 
+    def average(self):
+        sum=0
+        for a in self.marks:
+            sum += a
+        print("average marks", sum/3)    
+                
+
     def welcome(self):
         print("welcome student")
 
-s1=Student("arjun",98)
+s1=Student("arjun",[98,88,90])
 print(s1.name, s1.marks)
 print(Student.college)
-
-s2=Student("ankoor", 100)
-print(s2.name, s2.marks)
-
-print(Student.college)
 s1.welcome()
+s1.average()
+
+s2=Student("ankoor", [100,99,93])
+print(s2.name, s2.marks)
+print(Student.college)
 s2.welcome()
+s2.average()
+
 
 #attributes: appearance like color,type 1. Class.attr 2. obj.attr 
 # preference: obj attr > class attr
